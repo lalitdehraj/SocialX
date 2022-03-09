df63fdeb663fab708b79ec11af1f24b949106ccc
Lalit Dehraj <lalitrock97@gmail.com>
2022-03-09 16:00:57 +0530

    Nav Graph Implemented

---
+++ app/src/main/java/com/example/socialxtest/newsfeed/NewsFeed.kt
@@ -0,0 +1,15 @@
+package com.example.socialxtest.newsfeed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.socialxtest.R
+
+
+class NewsFeed : Fragment() {
+
+
+
+}
